7dddf8eeac828f1d3e3df4dd256d962a45810185
JabinWashburn <jabinwashburn@gmail.com>
2020-03-13 16:02:45 -0700

    finished working on the tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,31 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+let topics = [];
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+.then( response => {
+    topics = response.data.topics;
+    let parent = document.querySelector(".topics");
+    topics.forEach((topic) => {
+        newTopic = document.createElement("div");
+        newTopic.classList.add("tab");
+        newTopic.textContent = topic;
+        parent.appendChild(newTopic);
+    });
+})
+    .catch(error => {
+        console.log('This error is your fault Jabin fix it', error);
+    }); 
+
+function TopicTab (data){
+    const topic = document.createElement('div');
+
+    topic.textContent = data.topics;
+
+    topic.classList.add('tab');
+
+    return topic;
+} 
+    const newTab = document.querySelector('.topic');
+
